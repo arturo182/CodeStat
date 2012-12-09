c778ec8e3e6331889313faf5ebd7b47237b62730
arturo182 <arturo182@tlen.pl>
2012-12-09 13:52:32 +0100

    Restore portable settings.

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,18 @@
-#include <QApplication>
-
 #include "mainwindow.h"
+
+#include <QApplication>
+#include <QSettings>
 
 int main(int argc, char *argv[])
 {
+	QSettings::setDefaultFormat(QSettings::IniFormat);
+
 	QApplication app(argc, argv);
+
+	//cheat to get portable settings
+	QSettings::setPath(QSettings::IniFormat, QSettings::UserScope, qApp->applicationDirPath());
+	QApplication::setOrganizationName("data");
+	QApplication::setApplicationName("settings");
 
 	MainWindow wnd;
 	wnd.show();
